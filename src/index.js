9cd514614cc833f5a6c004c0beab3867225ce70f
Skladaniuk <skladanyuk1999@gmail.com>
2022-11-11 22:37:51 +0200

    coomit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
  
-    <BrowserRouter basename = '/Test-Task-Aleannlab'>
+    <BrowserRouter basename = '/Aleannlab-test-task'>
     <App />
     </BrowserRouter>
     
