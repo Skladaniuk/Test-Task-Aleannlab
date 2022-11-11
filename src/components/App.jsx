65df20661e8ea81351e0d91bb7d4f6b670d2694a
Skladaniuk <skladanyuk1999@gmail.com>
2022-11-11 17:19:14 +0200

    commit

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+import {Routes, Route} from 'react-router-dom';
+import { JobList } from './Views/JobList';
+import { JobInfo } from './Views/JobInfo';
+
+
+export const App = () => {
+
+
+  
+  
+
+  return (
+    <div>
+    <Routes>
+      <Route path = '/' element = {<JobList />}/>
+      <Route path = '/:jobId' element ={<JobInfo/>}/>
+     
+      </Routes>
+
+    </div>
+  );
+};
---
+++ src/components/WorksList/WorksList.jsx
@@ -0,0 +1,13 @@
+// import {Link,  useNavigate} from 'react-router-dom';
+// import { JobInfo } from 'components/Views/JobInfo';
+
+// export const WorksList = ({list}) => {
+
+//     const navigate = useNavigate()
+    
+  
+    
+//     return (
+//         <ul> {list.map(item => <Link   key = {item.id} > <li> {item.title} </li> </Link>)}</ul>
+//     )
+// }
